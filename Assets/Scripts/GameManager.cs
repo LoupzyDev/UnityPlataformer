8aa0072da06f72dbe537e71edb0b676e7456cfe2
Loupzy_Dev <eduardo.posadas@saei.mx>
2024-01-14 09:50:05 -0600

    14/01/2024 Created GameManager
    GameManager created
    Player Movement Modificed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public enum GameState {
+    None,
+    MainMenu,
+    Playing,
+    GameOver
+}
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager _instance;
+    GameState gameState;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Awake() {
+        _instance = this;
+        gameState = GameState.None;
+    }
+    private void Start() {
+        changeGameState(GameState.Playing);
+    }
+    public void changeGameState(GameState newstate) {
+        gameState = newstate;
+        switch (gameState) {
+            case GameState.None:
+                break;
+            case GameState.MainMenu:
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.GameOver:
+                break;
+        }
+    }
+    public GameState getGameState() {
+        return gameState;
+    }
+    public bool isPlaying() {
+        return gameState == GameState.Playing;
+    }
+    public void changeGameStateFromEditor(string newState) {
+        changeGameState((GameState)System.Enum.Parse(typeof(GameState), newState));
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float dashSpeed = 15f;  
+    [SerializeField] private float dashDuration = 1f;  
+    [SerializeField] private float jumpSpeed = 7f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+    private float dashTimeLeft = 0f;  
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+
+        if(GameManager._instance.isPlaying()) {
+
+            Movement();
+
+            if (Input.GetButtonDown("Jump") && isGrounded) {
+                Jump();
+            }
+            if (Input.GetKeyDown("k")) {
+                Dash();
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+
+        if (GameManager._instance.isPlaying()) {
+            CheckGrounded();
+            HandleDashCooldown();
+        }
+    }
+
+    private void Movement() {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float targetVelocity = moveX * moveSpeed;
+
+        if (Input.GetKey("k") && dashTimeLeft > 0) {
+            targetVelocity = moveX * dashSpeed;
+        }
+
+        float smoothTime = isGrounded ? 0.01f : 0.005f;
+
+        Vector2 velocity = rb.velocity;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocity, ref velocity.x, smoothTime);
+
+        rb.velocity = velocity;
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+    }
+
+    private void Dash() {
+        if (dashTimeLeft <= 0) {
+            dashTimeLeft = dashDuration;
+            
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+        }
+    }
+
+    private void CheckGrounded() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void HandleDashCooldown() {
+        if (dashTimeLeft > 0) {
+            dashTimeLeft -= Time.fixedDeltaTime;
+        }
     }
 }
